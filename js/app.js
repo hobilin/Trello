0f4deabfdf260412e31b3dbe4cdcb301100b3b7d
hobilin <paulibareyes@gmail.com>
2017-11-11 15:54:25 -0300

    versión 0.01

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-p
+var btnAdd = document.getElementById("btnAdd");
+document.getElementById("firstForm").style.display = "none";
+
+btnAdd.addEventListener("click", function(){
+ var container = document.getElementById("container")
+  var containerForm = document.getElementById("firstForm");
+  btnAdd.style.display = "none";
+  containerForm.style.display = "inline-block";
+
+});
